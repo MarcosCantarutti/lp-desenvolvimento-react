c5c6b4af080905994c8a46ca4a01599018ff07c2
Marcos <marcossandim21@gmail.com>
2023-09-16 19:12:57 -0300

    working page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './style/index.scss';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage';
-import Consultoria from './pages/Consultoria/Consultoria';
-import NotFound from './pages/NotFound/NotFound';
+// import MainPage from './pages/MainPage/MainPage';
+// import Consultoria from './pages/Consultoria/Consultoria';
+// import NotFound from './pages/NotFound/NotFound';
 import WorkingProgress from './pages/Working/WorkingProgress';
 
 function App() {
