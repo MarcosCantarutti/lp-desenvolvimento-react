3c60968e9ecd1717aaf0d4f65a989aad1f4a534a
marcos_cantarutti <marcossandim21@gmail.com>
2023-09-20 12:24:51 -0300

    implementado textos

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import './Button.scss';
 function Button(props) {
   return (
     <button className={props.className}>
-      <a href="https://wa.me/5534998372915" target="_blank">
+      <a href="https://wa.me/5534999457198" target="_blank">
         {props.children}
       </a>
     </button>
